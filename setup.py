b69b33e1c49d568d1ddc3b27dcfea08dc93fac7a
Erik Wright <erik@akoha.com>
2010-01-20 16:45:05 -0500

    Bump the version to publish a new egg (this version is South-enabled).

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='django-digest',
-    version='1.2',
+    version='1.3',
     description=('An implementation of HTTP Digest Authentication for Django.'),
     long_description=(
 """
