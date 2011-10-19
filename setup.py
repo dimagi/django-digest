c84bcc2ce20fd990d729792091e2c27426f8d12f
Jody McIntyre <jodym@trustcentric.com>
2011-10-19 11:36:55 -0400

    Bump version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='django-digest',
-    version='1.13',
+    version='1.14',
     description=('An implementation of HTTP Digest Authentication for Django.'),
     long_description=(
 """
